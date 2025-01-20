a22dd25793113edbda1c362ecd5de0ac571263fe
Elena Kharsova <elena.kharsova@gmail.com>
2025-01-20 14:51:06 +0100

    fix: correct out in the task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
 const path = require("node:path");
+const process = require("node:process")
 
 const {stdin, stdout} = process;
 
 const writableStream = fs.createWriteStream(path.join(__dirname, "textOUT.txt"));
 stdout.write("Hello, traveler!\nTell me about yourself:\n");
 stdin.on("data", (chunk) => { 
+  if(chunk.toString().trim() === "exit"){
+    console.log("Goodbye, traveler!");  
+    process.exit();
+  }
   writableStream.write(chunk);
-  process.exit();
 });
 stdin.on("error", (error)=>{
   console.log("Error", error.message);
+});
+
+process.on("SIGINT",()=>{
+  console.log("Goodbye, traveler!");  
+  process.exit();
 })
